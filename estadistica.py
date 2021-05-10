9b0e2e732943fdb00bba17b3bc32619489d5d399
Carlos che <car123che@gmail.com>
2021-05-10 13:01:33 -0600

    calculo mediana agregado

--- estadistica.py
+++ estadistica.py
@@ -13,6 +13,8 @@ suma = 0
 n = len(datos)
 varianza = 0
 desviacion = 0
+#ordenar datos
+datos.sort()
 #imprimir datos obtenidos
 print("*******************************LISTA DATOS*******************")
 print(datos, "\n")
@@ -23,6 +25,18 @@ for x in datos:
     suma += x
 media = suma/n
 print("La media es: ", media)
+
+#calculo mediana
+mediana  = 0
+if n%2 != 0: #si es impar
+    z = int((n+1)/2)
+    mediana = datos[z-1]
+else: #si es par
+    z = int(n/2)
+    datoUno = datos[z-1]
+    datosDos = datos[z]
+    mediana = (datoUno + datosDos) / 2
+print("La mediana es: ", mediana)
 
 #calculo varianza
 sumaCuadrados = 0
